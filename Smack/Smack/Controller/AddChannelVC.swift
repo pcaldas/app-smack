c6c8b69453657a749b9a58d89daf6a0ae55a589b
Paulo Caldas <paulocaldas@MacBook-Pro-de-Paulo.local>
2018-04-12 14:34:45 -0300

    add addChannelVC

---
+++ Smack/Smack/Controller/AddChannelVC.swift
@@ -0,0 +1,48 @@
+//
+//  AddChannelVC.swift
+//  Smack
+//
+//  Created by Paulo Caldas on 12/04/2018.
+//  Copyright © 2018 PMC. All rights reserved.
+//
+
+import UIKit
+
+class AddChannelVC: UIViewController {
+    
+    @IBOutlet weak var nameTxt: UITextField!
+    @IBOutlet weak var chanDesc: UITextField!
+    @IBOutlet weak var bgView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setUpView()
+    }
+
+    @IBAction func closeModalPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func createChannelPressed(_ sender: Any) {
+        
+        //guard let name = nameTxt.text, nameTxt.text != "" else { return }
+        //guard let chanDes = chanDesc.text , chanDesc.text != "" else { return }
+        
+        
+        
+    }
+    
+    func setUpView() {
+        nameTxt.attributedPlaceholder = NSAttributedString(string: "name", attributes: [NSAttributedStringKey.foregroundColor: smackPurplePlaceholder])
+        chanDesc.attributedPlaceholder = NSAttributedString(string: "description", attributes: [NSAttributedStringKey.foregroundColor: smackPurplePlaceholder])
+        
+        let closeTouch = UITapGestureRecognizer(target: self, action: #selector(ProfileVC.closeTap(_:)))
+        bgView.addGestureRecognizer(closeTouch)
+    }
+    
+    @objc func closeTap(_ recognizer: UITapGestureRecognizer) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
